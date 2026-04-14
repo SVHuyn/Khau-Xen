91d7e4287c05847bf7554c16bbf276939f53642b
huynh is alone <huynhronaldo123@gmail.com>
2026-04-14 17:03:24 +0700

    khauxen

---
+++ src/main/java/com/example/khauxenshop/KhauxenshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.khauxenshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhauxenshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhauxenshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/khauxenshop/KhauxenshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.khauxenshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhauxenshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
